60b5bc4145d0212f27d89f02d2aa52703ce6b249
prashanth ch. <prashanth@chegus.com>
2025-07-23 18:20:59 +0530

    understood the filter() in Stream as a intermediate Operation

---
+++ src/main/java/com/learn/java8/LearnStreamAPI/IntermediateOpsMethods/Readme.java
@@ -0,0 +1,19 @@
+package com.learn.java8.LearnStreamAPI.IntermediateOpsMethods;
+
+public class Readme {
+    /**
+     * every Intermediate Operations will return Stream<T>
+     * Intermediate Operations works Lazily
+     * create a Stream(lazy)
+     * 
+     * Intermadiate Ops methods
+     * filter(Predicate<T> p)
+     * map(Function<? super T, ? extend R> mapper)
+     * sorted()
+     * distint()
+     * flatMap(Function<? super T, ? extends Stream<? extends R>> mapper)
+     * limit(long maxSize)
+     * peek(Consumer<? super T> action)
+     * skip(long n)
+     */
+}
