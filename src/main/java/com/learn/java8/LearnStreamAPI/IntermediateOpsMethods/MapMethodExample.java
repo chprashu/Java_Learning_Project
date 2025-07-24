42f6247aefb08340d7dad21b7533e295f1543022
Chprshu <chprashanth618@gmail.com>
2025-07-24 22:46:09 +0530

    map method changes

--- src/main/java/com/learn/java8/LearnStreamAPI/IntermediateOpsMethods/MapMethodExample.java
+++ src/main/java/com/learn/java8/LearnStreamAPI/IntermediateOpsMethods/MapMethodExample.java
@@ -102,6 +102,68 @@ public class MapMethodExample {
             String[] parts = name.trim().toLowerCase().split("\\s+");
             return parts[0]+"."+parts[1]+"@example.com";
         }).forEach(System.out::println);
+        System.out.println();
+
+        /*
+        Masking Sensitive IP Addresses
+        You're given a list of IP addresses (IPv4) as List<String>. Some of these IPs belong to internal systems and need to be masked for logging purposes.
+        Write a Java method that:
+        Accepts a list of IP addresses.
+        Masks each IP address by replacing the last octet with "***" (e.g., "192.168.0.100" becomes "192.168.0.***").
+        Returns a new List<String> of the masked IPs using the Stream API and map() function.
+         */
+        List<String> ips = Arrays.asList(
+                "192.168.0.100",
+                "10.0.0.1",
+                "172.16.254.5",
+                "invalid.ip.address"
+        );
+
+        ips.stream().map(ip -> {
+            String[] parts = ip.split("\\.");
+            if(parts.length == 4){
+                parts[3] = "***";
+            }
+            return String.join(".", parts);
+        }).forEach(System.out::println);
+        System.out.println();
+
+        /*
+        Check Validity of IP Addresses Using
+        You are given a list of IP addresses (IPv4), represented as a List<String>. Write a Java method that:
+        Uses the Stream API with map() to transform each IP into true or false.
+
+        IP Validation Rules:
+        An IP address is valid if:
+        It contains exactly 4 parts separated by ".".
+        Each part is an integer between 0 and 255.
+        No leading zeros unless the number is 0 (e.g., "01" is invalid, but "0" is valid).
+         */
+        List<String> tempIps = Arrays.asList(
+                "192.168.1.1",
+                "255.255.255.255",
+                "256.100.50.25",
+                "123.045.067.089",
+                "10.0.0",
+                "1.2.3.4.5",
+                "abc.def.ghi.jkl",
+                "0.0.0.0",
+                "01.2.3.4"
+        );
+
+        tempIps.stream().map(ip ->{
+            String[] parts = ip.split("\\.");
+            if(parts.length < 4)
+                return false;
+            for(String part: parts){
+                if(!part.matches("\\d+")) return false;
+                if(part.length() > 1 && part.startsWith("0")) return false;
+                if(Integer.parseInt(part) > 255 || Integer.parseInt(part) < 0) return false;
+            }
+            return true;
+        }).forEach(System.out::println);
+
+
 
 
     }
