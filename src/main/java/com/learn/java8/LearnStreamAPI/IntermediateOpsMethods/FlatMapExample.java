900d2b7eddd980ca9c13a7f085e2c1217bc61205
prashanth ch. <prashanth@chegus.com>
2025-07-25 19:01:54 +0530

    flatMap learning in progress

---
+++ src/main/java/com/learn/java8/LearnStreamAPI/IntermediateOpsMethods/FlatMapExample.java
@@ -0,0 +1,14 @@
+package com.learn.java8.LearnStreamAPI.IntermediateOpsMethods;
+
+public class FlatMapExample {
+
+    /*
+     * flatMap() accepts Function(functional Interface) as a argument
+     * flatMap(Function<T, Stream<R>>)
+     * returns
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
