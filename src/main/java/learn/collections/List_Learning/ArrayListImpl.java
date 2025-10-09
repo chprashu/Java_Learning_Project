4de2ab9b97a3637ac6f26e553014d4820ec0ef58
prashanth ch. <prashanth@chegus.com>
2025-10-09 19:44:59 +0530

    started learning collection framework

---
+++ src/main/java/learn/collections/List_Learning/ArrayListImpl.java
@@ -0,0 +1,12 @@
+package learn.collections.List_Learning;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListImpl {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        System.out.println(list.add(1));
+        System.out.println(list.add(1));
+    }
+}
